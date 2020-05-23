ab67ea9db9b8bfd56dbf4263193d73a2796de610
Jeremy Schulman <jeremy.schulman@mlb.com>
2020-05-22 20:10:05 -0400

    docs

--- django3_saml2_nbplugin/__init__.py
+++ django3_saml2_nbplugin/__init__.py
@@ -1,5 +1,15 @@
-from django3_okta_saml2.config import SAML2_AUTH_CONFIG
+"""
+This purpose of this plugin is to provide the SAML2_AUTH_CONFIG configuration
+to the django3_okta_saml2 package. The .conf module defines SAML2_AUTH_CONFIG
+as a dictionary, and this plugin is used to copy the contents provided from the
+PLUGIN_CONFIG in configuration.py into SAML2_AUTH_CONFIG.
+
+Do not try to modify django.conf.settings.SAML2_AUTH_CONFIG since by the time
+this plugin is invoked the settings is already configured, and if you try
+settings.configure(SAML2_AUTH_CONFIG=user_config) an exception will be raised.
+"""
 from extras.plugins import PluginConfig
+from django3_okta_saml2.config import SAML2_AUTH_CONFIG
 
 
 class Django3AuthSAML2Plugin(PluginConfig):
