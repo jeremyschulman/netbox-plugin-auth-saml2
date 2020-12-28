f92edc79967cd71b970997ebc71cdbdd26c83f09
Jeremy Schulman <jeremy.schulman@mlb.com>
2020-12-28 11:43:47 -0500

    update setup.py for readme

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='netbox-plugin-auth-saml2',
-    version='2.0',
+    version='2.1',
     description='Netbox plugin for SAML2 auth',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jeremy Schulman',
     license='Apache 2.0',
     install_requires=[],
